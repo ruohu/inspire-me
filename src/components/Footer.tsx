ef366716e50044a60078f2731f8ea66a05599cb4
Ruoshi Hu <ruoshi.hu@gmail.com>
2024-03-11 17:52:31 +0100

    update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 const Footer: FC = () => {
   return (
     <Box bgcolor="primary.dark" sx={{ textAlign: "center", p: 1 }}>
-      <Typography color="common.white">2024</Typography>
+      <Typography color="common.white">&copy; 2024 Inpire Me - All rights reserved</Typography>
     </Box>
   )
 };
